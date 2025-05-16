03f1cb8b316eb1c721c32da702e0a7794c072936
Fernando Tinno <fernandotinno666@gmail.com>
2025-05-15 23:26:27 -0300

    fiz as instancias da classe produto

--- produto.py
+++ produto.py
@@ -1,3 +1,51 @@
 class Produto:
-    def __init__(self):
-        pass
+    def __init__(self,nome,marca,quantidade_estoque:int,preco:float):
+        self.__nome = nome
+        self.__marca = marca
+        self.__quantidade_estoque = quantidade_estoque
+        self.__ =preco = preco
+
+    @property
+    def _nome(self):
+        return self.__nome
+
+    @_nome.setter
+    def _nome(self, value):
+        self.__nome = value
+
+    @property
+    def _marca(self):
+        return self.__marca
+
+    @_marca.setter
+    def _marca(self, value):
+        self.__marca = value
+
+    @property
+    def _quantidade_estoque(self):
+        return self.__quantidade_estoque
+
+    @_quantidade_estoque.setter
+    def _quantidade_estoque(self, value):
+        self.__quantidade_estoque = value
+
+    @property
+    def _(self):
+        return self.__
+
+    @_.setter
+    def _(self, value):
+        self.__ = value
+
+    
+    
+        
+
+    
+
+
+notebook_1 = Produto('aspire ultimate','acer',25,2799.99)
+notebook_2 = Produto('future vision','positivo',1,3000.00)
+micro_ondas = Produto('hot wave','eletrolux',16,350.00)
+cama = Produto('cochão king','gazin',3,1749.99)
+        
